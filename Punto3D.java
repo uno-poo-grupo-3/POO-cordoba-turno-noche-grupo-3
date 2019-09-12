f6d77808b107e9d6b7e5c89b6d5cd6cb1c9b798d
Juan Picasso <jpicasso88@gmail.com>
2019-09-12 16:27:03 -0300

    Create Punto3D.java

---
+++ Punto3D.java
@@ -0,0 +1,33 @@
+package clase4;
+
+public class Punto3D extends Punto {
+	private double z;
+	
+	public static void main(String[] args) {
+		Punto3D p1 = new Punto3D(10, 4, 5);
+		System.out.println(p1.getZ());
+	}
+
+	public Punto3D(double z) {
+		super();
+		this.z = z;
+	}
+	
+	public Punto3D(double x, double y, double z) {
+		super(x, y);
+		this.setZ(z);
+	}
+
+	public double getZ() {
+		return z;
+	}
+
+	public void setZ(double z) {
+		this.z = z;
+	}
+
+	@Override
+	public String toString() {
+		return "Punto3D [z=" + z + "]";
+	}
+}
