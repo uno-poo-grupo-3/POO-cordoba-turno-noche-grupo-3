0549ca3bf5365ee9f723523b10aa07610a742730
Juan Picasso <jpicasso88@gmail.com>
2019-09-15 01:52:57 -0300

    Update SalarioFamiliar.java

--- SalarioFamiliar.java
+++ SalarioFamiliar.java
@@ -6,12 +6,10 @@ public class SalarioFamiliar {
 	private boolean casado;
 	public static final double PRECIO_POR_HIJO = 200;
 	public static final double PRECIO_SI_ES_CASADO = 100;
-	private double salario;
 	
 	public SalarioFamiliar(int hijos, boolean casado) {
 		this.hijos = hijos;
 		this.casado = casado;
-		this.setSalario();
 	}
 
 	public double getHijos() {
@@ -30,16 +28,13 @@ public class SalarioFamiliar {
 		this.casado = esCasado;
 	}
 	
-	public double getSalario() {
-		return this.salario;
-	}
 	
-	private void setSalario() {
+	public double calcularSalario() {
 		double salarioFamiliar = this.getHijos() * PRECIO_POR_HIJO;
 		if (this.isCasado()) {
 			salarioFamiliar += PRECIO_SI_ES_CASADO;
 		}
-		this.salario = salarioFamiliar;
+		return salarioFamiliar;
 	}
 	
 	
