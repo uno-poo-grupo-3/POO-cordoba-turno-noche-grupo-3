2dcc75b4f643e926db4d6f4ecc195f4a76a5e765
Juan Picasso <jpicasso88@gmail.com>
2019-09-15 01:44:47 -0300

    Create EmpleadoTemporario.java

---
+++ EmpleadoTemporario.java
@@ -0,0 +1,15 @@
+package clase5;
+
+public class EmpleadoTemporario extends Empleado {
+	
+	public EmpleadoTemporario(int horasTrabajadas, int hijos, boolean casado) {
+		super(200, horasTrabajadas, hijos, casado);
+	}
+	
+	@Override
+	public double calcularSalario() {
+		return 	(this.getHorasTrabajadas() * this.getPrecioPorHora()) +
+				(this.getSalarioFamiliar().getSalario());
+	}
+	
+}
