520e56cf41657290c0095d756cd5d0caed69ed46
Juan Picasso <jpicasso88@gmail.com>
2019-09-15 01:43:50 -0300

    Create EmpleadoPermanente.java

---
+++ EmpleadoPermanente.java
@@ -0,0 +1,48 @@
+package clase5;
+
+public class EmpleadoPermanente extends Empleado {
+	
+	private int antiguedad;
+	private double precioPorAntiguedad;
+
+	public EmpleadoPermanente(int horasTrabajadas, int antiguedad, int hijos, boolean casado) {
+		super(300, horasTrabajadas, hijos, casado);
+		this.setAntiguedad(antiguedad);
+		this.setPrecioPorAntiguedad(100);
+	}
+	
+	protected EmpleadoPermanente(double precioHora, int horasTrabajadas, int antiguedad, double precioAntiguedad, int hijos, boolean casado) {
+		super(precioHora, horasTrabajadas, hijos, casado);
+		this.setAntiguedad(antiguedad);
+		this.setPrecioPorAntiguedad(precioAntiguedad);
+	}
+	
+	public EmpleadoPermanente() {
+		super();
+	}
+	
+	public int getAntiguedad() {
+		return antiguedad;
+	}
+
+	public void setAntiguedad(int antiguedad) {
+		this.antiguedad = antiguedad;
+	}
+	
+	public void setPrecioPorAntiguedad(double precioPorAntiguedad) {
+		this.precioPorAntiguedad = precioPorAntiguedad;
+	}
+	
+	public double getPrecioPorAntiguedad() {
+		return this.precioPorAntiguedad;
+	}
+	
+	
+	@Override
+	public double calcularSalario() {
+		return 	(this.getHorasTrabajadas() * this.getPrecioPorHora()) + 
+				(this.getAntiguedad() * this.getPrecioPorAntiguedad()) +
+				(this.getSalarioFamiliar().getSalario());
+	}
+
+}
