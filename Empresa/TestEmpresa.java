f20394066ce35a40e24601189215e7950bb1126b
aguzj1j1 <50180644+aguzj1j1@users.noreply.github.com>
2019-09-22 22:41:38 -0300

    en beta, agregado de asserts dni
    falta que ande practicamente. lo termino mañana

--- Empresa/TestEmpresa.java
+++ Empresa/TestEmpresa.java
@@ -13,12 +13,15 @@ class TestEmpresa {
 	@Test
 	void test() {
 		ArrayList<Empleado> empleados = new ArrayList<Empleado>();
-		empleados.add(new EmpleadoPermanente(10, LocalDate.of(1999, Month.JANUARY, 26), 2, true));
-		empleados.add(new EmpleadoTemporario(30, 1, false));
+		Empleado empleado= new Empleado(50, 40,2, true, 156458);
+		empleados.add(new EmpleadoPermanente(10, LocalDate.of(1999, Month.JANUARY, 26), 2, true,388289));
+		empleados.add(new EmpleadoTemporario(30, 1, false,182930));
 		Empresa miEmpresa = new Empresa(123, "Emp S.A", empleados);
-		miEmpresa.agregarEmpleado(new Gerente(3, LocalDate.of(2009, Month.NOVEMBER, 26), 4, true));
+		miEmpresa.agregarEmpleado(new Gerente(3, LocalDate.of(2009, Month.NOVEMBER, 26), 4, true,123232));
 		double liquidacion = miEmpresa.montoTotal();
+		Integer dni = empleado.getDni();
 		assertEquals(15300, liquidacion, 0);
+		assertEquals(123232, dni, 0);
 	}
 
 }
