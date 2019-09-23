a3f27e2e8cf29a8ac061ea1d097d83cdc428c512
aguzj1j1 <50180644+aguzj1j1@users.noreply.github.com>
2019-09-22 22:40:00 -0300

    Agregado de dni

--- Empresa/Gerente.java
+++ Empresa/Gerente.java
@@ -1,9 +1,9 @@
 package clase5;
-
+import java.time.LocalDate;
 public final class Gerente extends EmpleadoPermanente {
 
-	public Gerente(int horasTrabajadas, int antiguedad, int hijos, boolean casado) {
-		super(400, horasTrabajadas, antiguedad, 150, hijos, casado);
+	public Gerente(int horasTrabajadas, LocalDate antiguedad, int hijos, boolean casado, Integer dni) {
+		super(400, horasTrabajadas, antiguedad, 150, hijos, casado,dni);
 	}
 	
 }
