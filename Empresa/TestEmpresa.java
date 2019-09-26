4e43d7d4498000b8ffabff3accd274545b3217d9
Juan Picasso <jpicasso88@gmail.com>
2019-09-26 15:19:31 -0300

    Update TestEmpresa.java

--- Empresa/TestEmpresa.java
+++ Empresa/TestEmpresa.java
@@ -11,17 +11,21 @@ import org.junit.jupiter.api.Test;
 class TestEmpresa {
 
 	@Test
-	void test() {
+	void testEmpresaMontoTotal() {
 		ArrayList<Empleado> empleados = new ArrayList<Empleado>();
-		Empleado empleado= new Empleado(50, 40,2, true, 156458);
 		empleados.add(new EmpleadoPermanente(10, LocalDate.of(1999, Month.JANUARY, 26), 2, true,388289));
 		empleados.add(new EmpleadoTemporario(30, 1, false,182930));
 		Empresa miEmpresa = new Empresa(123, "Emp S.A", empleados);
 		miEmpresa.agregarEmpleado(new Gerente(3, LocalDate.of(2009, Month.NOVEMBER, 26), 4, true,123232));
 		double liquidacion = miEmpresa.montoTotal();
-		Integer dni = empleado.getDni();
 		assertEquals(15300, liquidacion, 0);
-		assertEquals(123232, dni, 0);
+	}
+	
+	@Test
+	void testEmpleadoEquals() {
+		Empleado e1 = new EmpleadoPermanente(10, LocalDate.of(1999, Month.JANUARY, 26), 2, true, 123);
+		Empleado e2 = new EmpleadoPermanente(25, LocalDate.of(1999, Month.SEPTEMBER, 26), 5, false, 123);
+		assertEquals(e1, e2);
 	}
 
 }
