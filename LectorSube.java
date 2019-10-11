ca28fa02a3ee45391326e7eeda741288e9cb9d89
aguzj1j1 <50180644+aguzj1j1@users.noreply.github.com>
2019-10-11 18:14:35 -0300

    Update LectorSube.java

--- LectorSube.java
+++ LectorSube.java
@@ -1,30 +1,51 @@
 package sube;
 
 public abstract class LectorSube {
-	private float saldo=100;
 	private int tramo=0; //mepa que no va
 	private  float valor;
 	private int cantidadDePasada;
 	private boolean esBeneficiarioTarifaSocial;
 
-	public LectorSube(float tramo,int  saldo, int cantidadDePasada, boolean esBeneficiarioTarifaSocial) {
-		setEsBeneficiarioTarifaSocial(esBeneficiarioTarifaSocial);
+	public LectorSube(float saldo,int  tramo, int cantidadDePasada, boolean esBeneficiarioTarifaSocial) {
 		setCantidadDePasada(cantidadDePasada);
+		setEsBeneficiarioTarifaSocial(esBeneficiarioTarifaSocial);
+		setSaldo(saldo);
+		setTramo(tramo);
+		setValor(valor);
 	}
 	private void setValor(float valor) {
-		this.valor = (esBeneficiarioTarifaSocial)?new TarifaSocial(valor).getValorPasaje():(cantidadDePasada!=0)?getValorPorTramo(tramo,cantidadDePasada):valor;
+		System.out.println("pase");
+		if(esBeneficiarioTarifaSocial) {
+			new TarifaSocial(valor);
+		}
+			
+		this.valor = (cantidadDePasada!=0)?getValorPorTramo(tramo,cantidadDePasada,esBeneficiarioTarifaSocial):valor;
+		System.out.println("soy valor" + this.valor);
+	}
+	public float getValor() {
+		return valor;
 	}
 	private void setCantidadDePasada(int cantidadDePasada) {
+		System.out.println("pase1");
 		this.cantidadDePasada = cantidadDePasada;
+		System.out.println("Soy cantidad de pasada " + this.cantidadDePasada);
 	}
 	private void setEsBeneficiarioTarifaSocial(boolean esBeneficiarioTarifaSocial) {
+		System.out.println("pase2");
 		this.esBeneficiarioTarifaSocial = esBeneficiarioTarifaSocial;
+		System.out.println("soy tarifa social" + this.esBeneficiarioTarifaSocial);
 	}
-	public float getValorPorTramo(int tramo,int cantidadDePasada) {
+	public void setTramo(int tramo) {
+		System.out.println("pase3");
+		this.tramo = tramo;
+		System.out.println("soy tramo" + this.tramo);
+	}
+	public float getValorPorTramo(int tramo,int cantidadDePasada,boolean esBeneficiarioTarifaSocial) {
 		float valorPorTramo = 0;
 		switch(tramo) {
 		case 1:
-			valorPorTramo= 19/cantidadDePasada;
+			valorPorTramo= 19/cantidadDePasada; 
+					
 			break;
 		case 2:
 			valorPorTramo= 21/cantidadDePasada;
@@ -33,10 +54,13 @@ public abstract class LectorSube {
 			valorPorTramo= 23/cantidadDePasada;
 			break;
 		}
+		if (esBeneficiarioTarifaSocial) {
+			valorPorTramo =valorPorTramo*100/60;
+		}
 		return valorPorTramo;
 	}
 	public void setSaldo(float saldo) {
-		this.saldo = saldo-valor;
+		System.out.println("pase4");
 	}
 	
 
