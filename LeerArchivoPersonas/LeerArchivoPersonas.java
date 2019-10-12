3afc73def7be08d2178892071a9dfc4fa4d40dde
Juan Picasso <jpicasso88@gmail.com>
2019-10-12 19:29:37 -0300

    Create LeerArchivoPersonas.java

---
+++ LeerArchivoPersonas/LeerArchivoPersonas.java
@@ -0,0 +1,58 @@
+package leerArchivoPersonas;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.stream.Collectors;
+
+public class LeerArchivoPersonas {
+	
+	public static LinkedList<Persona> getPersonas(String ruta) {
+		try(FileReader fileReader = new FileReader(ruta)) {
+			BufferedReader buffer = new BufferedReader(fileReader);
+			String linea = "";
+			LinkedList<Persona> personas = new LinkedList<Persona>();
+			while((linea = buffer.readLine()) != null) {
+				String[] partes = linea.split(" ");
+				personas.add(new Persona(
+						Integer.parseInt(partes[0]), 
+						partes[1], 
+						Integer.parseInt(partes[2])
+					));
+			}
+			return personas;
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+		return null; 
+	}
+	
+	public static LinkedList<Persona> getPersonasOrdenadasPorDni(LinkedList<Persona> personas) {
+		LinkedList<Persona> personasOrdenadasPorDni = new LinkedList<Persona>(personas);
+		Collections.sort(personasOrdenadasPorDni, new OrdenarPersonaPorDni());
+		return personasOrdenadasPorDni;
+	}
+	
+	public static LinkedList<Persona> getPersonasMayoresAEdad(LinkedList<Persona> personas, Integer edad) {
+		LinkedList<Persona> copiaPersonas = new LinkedList<Persona>(personas);
+		return copiaPersonas.stream()
+				.filter(persona -> persona.getEdad() > edad)
+				.collect(Collectors.toCollection(LinkedList::new));
+	}
+	
+	public static void listarPersonas(LinkedList<Persona> personas, String archivoSalida) {
+			try(BufferedWriter bw = new BufferedWriter(new FileWriter(archivoSalida))) {
+				for(Persona persona : personas) {
+					bw.write(persona.toString());
+					bw.newLine();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	
+}
