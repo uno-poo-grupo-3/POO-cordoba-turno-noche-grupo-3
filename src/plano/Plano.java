05b4770e34e21cf87782f7ad128e893c43464db1
Juan Picasso <jpicasso88@gmail.com>
2019-10-17 19:46:17 -0300

    Update Plano.java

--- src/plano/Plano.java
+++ src/plano/Plano.java
@@ -5,21 +5,22 @@ import clase4.Punto;
 
 public class Plano {
 	
-	private ArrayList<Punto> puntos;
-	
 	public static void main(String[] args) {
 		ArrayList<Punto> puntos = new ArrayList<Punto>();
 		puntos.add(new Punto(1,2));
 		puntos.add(new Punto(-3,-4));
 		puntos.add(new Punto(-1,1));
 		puntos.add(new Punto(5,-1));
+		Plano plano = new Plano(puntos);
 		
-		for(Punto p : puntos) {
+		for(Punto p : plano.getPuntos()) {
 			Cuadrante cuadrante = p.getCuadrante();
 			System.out.println(cuadrante);
 		}
 		
 	}
+	
+	private ArrayList<Punto> puntos;
 	
 	public Plano(ArrayList<Punto> puntos) {
 		this.setPuntos(puntos);
