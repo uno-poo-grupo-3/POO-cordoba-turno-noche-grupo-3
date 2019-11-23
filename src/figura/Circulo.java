0277a24135b3be2a8e7c9b5e7e701837515adede
Juan Picasso <juan.picasso@almundo.com>
2019-11-23 18:52:42 -0300

    agrego sumatoria de areas

--- src/figura/Circulo.java
+++ src/figura/Circulo.java
@@ -20,5 +20,4 @@ public class Circulo implements Figura {
 		this.radio = radio;
 	}
 	
-	
 }
--- src/figura/TestInterface.java
+++ src/figura/TestInterface.java
@@ -1,10 +1,19 @@
 package figura;
 
+import java.util.ArrayList;
+
 public class TestInterface {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		ArrayList<Figura> figuras = new ArrayList<Figura>();
+		figuras.add(new Cuadrado(4.5));
+		figuras.add(new Circulo(6.4));
+		figuras.add(new Rectangulo(2.4, 8.4));
+		double totalArea = 0;
+		for(Figura figura : figuras) {
+			totalArea += figura.area();
+		}
+		System.out.println(totalArea);
 	}
 
 }
